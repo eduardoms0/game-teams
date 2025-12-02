83ec0f6cbfb5035da100280c725b868686a77151
Eduardo Machado <eduardomachadodesouza123@gmail.com>
2025-12-01 21:38:57 -0300

    criado o thema

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
 import { Groups } from '@screens/groups';
-
+import { ThemeProvider} from "styled-components/native";
+import theme from './src/theme';
 
 export default function App() {
   return (
+    <themeProvier theme= {theme}>
     <Groups/>
+    </themeProvier>
   );
 }
 
